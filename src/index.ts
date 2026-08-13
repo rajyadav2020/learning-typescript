ec7b50462b2eca98ab9493c0f768139eca5c6861
rajyadav2020 <rajyadavv176@gmail.com>
2026-08-13 15:15:10 +0530

    learned about the type inference which is actually a very good feature of typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 //so the type script variables are defined with their types itself whether they are numbers,string
 
 let nam: string = "raj";
-let age: number = 20;
 let isstudent: boolean = true;
 
 
@@ -13,3 +12,15 @@ let username: string = "rajkumarhere";
 //defining the numbers
 
 let population :number = 20;
+
+//now comes the type inference
+
+//under this we dont need to explicitly write the type
+
+//like\
+let age : number = 20;
+ 
+//this above can also be written as 
+let age = 20 //here actually typescript automatically understands that age is offcourse a number , this is called the type inference
+
+
