8f756e1ead4216fc5181b1fd26152ef6e4fdb900
rajyadav2020 <rajyadavv176@gmail.com>
2026-08-13 15:10:24 +0530

    learned how to declare the strings

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+//so the type script variables are defined with their types itself whether they are numbers,string
+
+let nam: string = "raj";
+let age: number = 20;
+let isstudent: boolean = true;
+
+
+//defining the strings
+
+let username: string = "rajkumarhere";
